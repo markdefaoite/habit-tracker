e226132b103eaa36131989c38d7534cb26569e5d
markdefaoite <markdefaoite@hotmail.com>
2024-06-26 17:15:56 -0400

    completing lesson 331

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from datetime import datetime
 
+"""
+This excercise comes from the course on udemy "100 days of code: the Complete Python Pro Bootcamp" 
+This is not so much a project as it is an exercise in using the requests module
+"""
+
 pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = "markdefaoite"
 TOKEN = "mnbv96385yrtew145nbvc"
@@ -39,6 +44,7 @@ headers = {
 post_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
 
 today = datetime.now()
+# today.strftime("%Y%m%d")
 pixel_config = {
     "date": today.strftime("%Y%m%d"),
     "quantity": "4.5",
@@ -47,3 +53,21 @@ pixel_config = {
 # response = requests.post(url=post_pixel_endpoint, json=pixel_config, headers=headers)
 # print(response.text)
 
+# Edit yesterday's entry
+
+yesterday = datetime(year=2024, month=6, day=25)
+
+update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{yesterday.strftime("%Y%m%d")}"
+
+put_pixel_config = {
+    "quantity": "2.5"
+}
+
+# response = requests.put(url=update_pixel_endpoint, json=put_pixel_config, headers=headers)
+# print(response.text)
+
+
+# Delete yesterday's entry
+
+# response = requests.delete(url=update_pixel_endpoint, headers=headers)
+# print(response.text)
